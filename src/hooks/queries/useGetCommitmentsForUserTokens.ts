2d481cf2b2ab53adc16482d38697efca00c8b007
andreskebe <andres@teller.org>
2024-04-03 15:30:35 -0400

    Basic queries

---
+++ src/hooks/queries/useGetCommitmentsForUserTokens.ts
@@ -0,0 +1,78 @@
+import { useQuery } from "@tanstack/react-query";
+import request, { gql } from "graphql-request";
+import { UserToken, useGetUserTokens } from "../useGetUserTokens";
+import { useGraphURL } from "../useGraphURL";
+import { useEffect, useMemo, useState } from "react";
+
+interface Commitment {
+  collateralToken: {
+    address: string;
+  };
+}
+
+export const useGetCommitmentsForUserTokens = () => {
+  const [commitmentsData, setCommitmentsData] = useState<any[]>([]);
+  const [loading, setLoading] = useState(true);
+  const graphURL = useGraphURL();
+  const { userTokens } = useGetUserTokens();
+
+  const hasTokens = userTokens.length > 0;
+
+  const userTokenCommitments = useMemo(
+    () =>
+      gql`
+        query commitmentsForUserTokens {
+          commitments(
+            where: {
+              collateralToken_: { address_in: ${JSON.stringify(
+                Array.from(new Set(userTokens.map((token) => token.address)))
+              )} }
+              status: "Active"
+              committedAmount_gt: "0"
+            }
+            orderBy: maxPrincipalPerCollateralAmount
+
+          ) {
+            collateralToken {
+              address
+            }
+          }
+        }
+      `,
+    [userTokens]
+  );
+
+  const { data } = useQuery({
+    queryKey: ["commitmentsForUserTokens"],
+    queryFn: async () => request(graphURL, userTokenCommitments),
+    enabled: !!hasTokens,
+  }) as { data: { commitments: Commitment[] }; isLoading: boolean };
+
+  useEffect(() => {
+    if (data?.commitments) {
+      const userCommitments = data.commitments.reduce((acc, current) => {
+        if (
+          acc?.find(
+            (commitment) =>
+              commitment?.address === current?.collateralToken?.address
+          )
+        ) {
+          return acc;
+        } else
+          acc.push(
+            userTokens.find(
+              (token) => token?.address === current?.collateralToken?.address
+            )
+          );
+        return acc;
+      }, [] as UserToken[]);
+      setCommitmentsData(userCommitments);
+      setLoading(false);
+    }
+  }, [data]);
+
+  return useMemo(
+    () => ({ commitmentsData, loading }),
+    [commitmentsData, loading]
+  );
+};
