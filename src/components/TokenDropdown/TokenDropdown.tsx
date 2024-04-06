57cdb366323fd8911d153b1e55b2eb014157af6a
andreskebe <andres@teller.org>
2024-04-06 15:53:35 -0400

    Token dropdown component

---
+++ src/components/TokenDropdown/TokenDropdown.tsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import cx from "classnames";
+
+import { UserToken } from "../../hooks/useGetUserTokens";
+import TokenLogo from "../TokenLogo";
+import caret from "../../assets/caret.svg";
+
+import "./tokenDropdown.scss";
+import { useGetBorrowSectionContext } from "../../pages/BorrowSection/BorrowSectionContext";
+import useOutsideClick from "../../hooks/useOutsideClick";
+
+interface TokenDropdownProps {
+  tokens: UserToken[];
+  selectedToken: UserToken;
+}
+
+interface TokenDropdownButtonProps {
+  token: UserToken;
+  onClick?: (token: UserToken) => void;
+}
+
+const TokenDropdownRow: React.FC<TokenDropdownButtonProps> = ({
+  token,
+  onClick,
+}) => (
+  <div className="token-dropdown--row" onClick={() => onClick?.(token)}>
+    <TokenLogo logoUrl={token?.logo} size={32} />
+    <div className="token-info">
+      <span>{token?.symbol}</span>
+      <span>
+        Balance: {Number(token?.balance).toFixed(3)} {token?.symbol}
+      </span>
+    </div>
+  </div>
+);
+
+const TokenDropdown: React.FC<TokenDropdownProps> = ({
+  tokens,
+  selectedToken: token,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { setSelectedCollateralToken } = useGetBorrowSectionContext();
+
+  const onTokenDropdownRowClick = (token: UserToken) => {
+    setSelectedCollateralToken(token);
+    setIsOpen(false);
+  };
+
+  const ref = useOutsideClick(() => setIsOpen(false));
+
+  return (
+    <div className="token-dropdown">
+      <div
+        className={cx("token-dropdown--row-container", isOpen && "opened")}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <TokenDropdownRow token={token} />
+        <div className={cx("caret", isOpen && "opened")}>
+          <img src={caret} />
+        </div>
+      </div>
+      {isOpen && tokens.length > 0 && (
+        <div className="token-dropdown--tokens" ref={ref}>
+          {tokens.map((token) => (
+            <TokenDropdownRow
+              token={token}
+              key={token.address}
+              onClick={onTokenDropdownRowClick}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default TokenDropdown;
