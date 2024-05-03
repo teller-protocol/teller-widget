5668ba1cae74bc60c357994ee56ef06ac8ec2248
andreskebe <andres@teller.org>
2024-05-02 21:35:00 -0400

    Create dropdown component

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,81 @@
+import cx from "classnames";
+import { useState } from "react";
+
+import caret from "../../assets/down-caret.svg";
+
+import useOutsideClick from "../../hooks/useOutsideClick";
+import "./dropdown.scss";
+
+export interface DropdownOption<L = React.ReactNode> {
+  label: string;
+  value: string;
+}
+
+interface DropdownProps {
+  options: DropdownOption[];
+  selectedOption: DropdownOption;
+  onChange: (option: DropdownOption) => void;
+  label?: string;
+  readonly?: boolean;
+}
+
+interface DropdownButtonProps {
+  option: DropdownOption;
+  onClick?: (token: DropdownOption) => void;
+}
+
+const DropdownRow: React.FC<DropdownButtonProps> = ({ option, onClick }) => (
+  <div className="token-dropdown--row" onClick={() => onClick?.(option)}>
+    <div className="token-info">
+      <div className="paragraph">{option?.label}</div>
+    </div>
+  </div>
+);
+
+const Dropdown: React.FC<DropdownProps> = ({
+  options,
+  selectedOption,
+  onChange,
+  label,
+  readonly,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const onDropdownRowClick = (option: DropdownOption) => {
+    onChange(option);
+    setIsOpen(false);
+  };
+
+  const ref = useOutsideClick(() => setIsOpen(false));
+
+  return (
+    <>
+      {label && <label className="section-title">{label}</label>}
+      <div className="token-dropdown" ref={ref}>
+        <div
+          className={cx("token-dropdown--row-container", isOpen && "opened")}
+          onClick={() => !readonly && setIsOpen(!isOpen)}
+        >
+          <DropdownRow option={selectedOption} />
+          {!readonly && (
+            <div className={cx("caret", isOpen && "opened")}>
+              <img src={caret} />
+            </div>
+          )}
+        </div>
+        {isOpen && options.length > 0 && (
+          <div className="token-dropdown--tokens">
+            {options.map((option) => (
+              <DropdownRow
+                option={option}
+                key={option.value}
+                onClick={onDropdownRowClick}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Dropdown;
