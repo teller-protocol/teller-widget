51416b2836815ef83d22cdf8c283be9e264a17e2
andreskebe <andres@teller.org>
2024-06-26 13:53:44 -0400

    Revert to centralized URLs

--- src/constants/graphEndpoints.ts
+++ src/constants/graphEndpoints.ts
@@ -2,11 +2,11 @@ import { arbitrum, base, goerli, mainnet, polygon, sepolia } from "viem/chains";
 
 export const GRAPH_ENDPOINTS: { [chainId: number]: string | undefined } = {
   [mainnet.id]:
-    "https://gateway-arbitrum.network.thegraph.com/api/b90c8484d1ba2492692f12151469678a/subgraphs/id/B6FW3z7yLTJKVuqz6kLDJAwJru1T89j4ww5JfY3GYX8F",
+    "https://api.studio.thegraph.com/proxy/51204/tellerv2-mainnet/version/latest",
   [polygon.id]:
-    "https://gateway-arbitrum.network.thegraph.com/api/b90c8484d1ba2492692f12151469678a/subgraphs/id/8bjHtQZ9PZUMQAbCGJw5Zx2SbZZY2LQz8WH3rURzN5do",
+    "https://api.studio.thegraph.com/proxy/36377/tellerv2-polygon/version/latest",
   [arbitrum.id]:
-    "https://api.studio.thegraph.com/query/51204/tellerv2-arbitrum/v0.4.21-16",
+    "https://api.studio.thegraph.com/proxy/51204/tellerv2-arbitrum/version/latest",
   [base.id]:
-    "https://gateway-arbitrum.network.thegraph.com/api/b90c8484d1ba2492692f12151469678a/subgraphs/id/ArZjkaosc5rmBoYqprFWomvnjKSFMGkPFD7yuaZGuqQo",
+    "https://api.studio.thegraph.com/query/36377/tellerv2-base/version/latest",
 };
