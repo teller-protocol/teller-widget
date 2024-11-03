dff4447a6e3c6d3b975bd30c68074b0d3d4bc693
andreskebe <andres@teller.org>
2024-11-03 10:25:37 -0500

    Return query

--- src/hooks/queries/useGetCommitmentsForCollateralToken.ts
+++ src/hooks/queries/useGetCommitmentsForCollateralToken.ts
@@ -76,7 +76,7 @@ export const useGetCommitmentsForCollateralToken = (
                 { commitmentBorrowers: [] }
               ]
             }`
-                : { commitmentBorrowers: [] }
+                : `{ commitmentBorrowers: [] }`
             }
           ]
         },
