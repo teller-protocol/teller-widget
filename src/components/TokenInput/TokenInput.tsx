25e71fbcf93026176d4259d650b1cedd140c82b5
gmryan.eth <45776981+rbcp18@users.noreply.github.com>
2025-02-06 13:03:43 +0000

    Update TokenInput.tsx incorrect change, revert back

--- src/components/TokenInput/TokenInput.tsx
+++ src/components/TokenInput/TokenInput.tsx
@@ -1,109 +1,129 @@
-@import "../../tokens.scss";
+import { useAccount, useBalance, useReadContract } from "wagmi";
+import { UserToken } from "../../hooks/useGetUserTokens";
+import { AddressStringType } from "../../types/addressStringType";
+import { erc20Abi, formatUnits, parseUnits } from "viem";
+import { SubgraphTokenType } from "../../hooks/queries/useGetCommitmentsForCollateralToken";
 
-.token-input-container {
-  .token-input {
-    margin-top: $s-small;
-    padding: 10px 12px;
-    border: 0.5px solid $border-color-secondary;
-    border-radius: $border-radius;
-    background-color: $bg-light;
+import "./tokenInput.scss";
+import TokenLogo from "../TokenLogo";
+import defaultTokenImage from "../../assets/generic_token-icon.svg";
+import { useState } from "react";
 
-    display: flex;
+export type TokenInputType = {
+  value?: number;
+  token?: SubgraphTokenType;
+  valueBI?: bigint;
+};
 
-    input {
-      background: none;
-      border: none;
-      padding: 0;
-      cursor: text;
-      outline: inherit;
-
-      font-size: 13px;
-      font-family: "Roboto";
-
-      flex-grow: 1;
-
-      &[readonly] {
-        cursor: default;
-      }
-    }
-
-    input[type="number"] {
-      -moz-appearance: textfield;
-    }
-
-    input::-webkit-outer-spin-button,
-    input::-webkit-inner-spin-button {
-      -webkit-appearance: none;
-      margin: 0;
-    }
-
-    .max-button {
-      font-family: "Roboto";
-      font-size: 12px;
-      color: rgba(0, 70, 68, 1);
-      background-color: rgba(228, 249, 248, 1);
-      padding: 2px 9px;
-      border-radius: $border-radius;
-      margin-right: $s-small;
-
-      cursor: pointer;
-    }
-
-    .token-info {
-      border-left: 0.5px solid $border-color-secondary;
-      margin-top: -10px;
-      margin-bottom: -10px;
-      display: flex;
-      align-items: center;
-
-      padding-left: .3rem;
-
-      .token-logo {
-        margin-right: .22rem;
-      }
-
-      &-symbol {
-        font-family: "Roboto";
-        font-size: 11px;
-      }
-    }
-  }
-
-  .sublabel.section-sub-title {
-    margin-top: $s-small;
-    color: $text-secondary;
-    text-align: right;
-  }
-
-  .sublabel-upper.section-sub-title {
-    color: $text-secondary;
-    text-align: right;
-  }
-
-  .label-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-
-  .rollover-svg .outer {
-    fill: none;             
-    stroke: #3D8974;
-    stroke-width: 1.5;
-    overflow: visible;
-  }
-
-  .rollover-svg .check {
-    fill: #3D8974;          
-    stroke: #3D8974;        
-    stroke-width: 4;        
-    overflow: visible;
-  }
-
-
-  .tooltip-svg {
-    stroke: $text-secondary;
-    overflow: visible;
-  }
-
+interface TokenInputProps {
+  tokenValue: TokenInputType;
+  imageUrl: string | null;
+  label?: React.ReactNode;
+  sublabel?: string;
+  sublabelUpper?: React.ReactNode;
+  maxAmount?: number;
+  showMaxButton?: boolean;
+  onChange?: (value: TokenInputType) => void;
+  readonly?: boolean;
+  limitToMax?: boolean;
+  min?: boolean;
+  minAmount?: bigint;
 }
+
+const TokenInput: React.FC<TokenInputProps> = ({
+  tokenValue,
+  label,
+  maxAmount,
+  imageUrl,
+  sublabel,
+  sublabelUpper,
+  onChange,
+  readonly,
+  showMaxButton = true,
+  limitToMax = false,
+  min = false,
+  minAmount,
+}) => {
+  const [isMin, setIsMin] = useState(false);
+
+  const maxValueBigInt = parseUnits(
+    (maxAmount ?? 0).toLocaleString('fullwide', { useGrouping: false }),
+    Number(tokenValue.token?.decimals) || 0
+  );
+
+  const setMaxValue = () => {
+    if (isMin) {
+      onChange?.({
+        ...tokenValue,
+        value: Number(
+          formatUnits(minAmount ?? 0n, tokenValue.token?.decimals ?? 0)
+        ),
+        valueBI: minAmount,
+      });
+    } else {
+      onChange?.({
+        ...tokenValue,
+        value: maxAmount,
+        valueBI: maxValueBigInt,
+      });
+    }
+    min && setIsMin(!isMin);
+  };
+
+  const handleChange = (e: React.FormEvent<HTMLInputElement>) => {
+    if (!e.currentTarget.value) {
+      onChange?.({
+        ...tokenValue,
+        value: undefined,
+        valueBI: BigInt(0),
+      });
+      return;
+    }
+    if (limitToMax && maxAmount) {
+      if (Number(e.currentTarget.value) > maxAmount) {
+        setMaxValue();
+        return;
+      }
+    }
+    onChange?.({
+      ...tokenValue,
+      value: Number(e.currentTarget.value),
+      valueBI: parseUnits(
+        e.currentTarget.value,
+        tokenValue.token?.decimals ?? 0
+      ),
+    });
+  };
+
+  const logoUrl = imageUrl ? imageUrl : defaultTokenImage;
+
+  return (
+    <div className="token-input-container">
+      <div className="label-container">
+        {label && <label className="section-title">{label}</label>}
+        {sublabelUpper && <label className="sublabel-upper section-sub-title">{sublabelUpper}</label>}
+      </div>
+      <div className="token-input">
+        <input
+          type="number"
+          value={tokenValue?.value}
+          max={Number(tokenValue.value)}
+          onChange={handleChange}
+          readOnly={readonly}
+        />
+        {showMaxButton && !!maxAmount && (
+          <div className="max-button" onClick={setMaxValue}>
+            {isMin ? "MIN" : "MAX"}
+          </div>
+        )}
+        <div className="token-info">
+          <TokenLogo logoUrl={logoUrl} size={16} />
+          <span className="token-info-symbol">{tokenValue.token?.symbol}</span>
+        </div>
+      </div>
+      {sublabel && <div className="sublabel section-sub-title">{sublabel}</div>}
+    </div>
+  );
+};
+
+export default TokenInput;
