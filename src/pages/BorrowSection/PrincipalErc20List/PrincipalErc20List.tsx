c021a11c726d8e9abde6605855a2b0547d90adf2
rbcp18 <8903256-rbcp18@users.noreply.replit.com>
2025-02-08 16:16:55 +0000

    User checkpoint: Refactor `PrincipalErc20List` to display aggregated committed amounts for ERC20 tokens using `CollateralTokenRow` component.

--- src/pages/BorrowSection/PrincipalErc20List/PrincipalErc20List.tsx
+++ src/pages/BorrowSection/PrincipalErc20List/PrincipalErc20List.tsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { useGetBorrowSectionContext } from "../BorrowSectionContext";
+import CollateralTokenRow from "../../../components/CollateralTokenRow";
 import "./principalErc20List.scss";
 
 const PrincipalErc20List: React.FC = () => {
+  const { principalErc20Tokens } = useGetBorrowSectionContext();
+
   return (
     <div className="principal-erc20-list">
-      erc20s
+      {principalErc20Tokens.map((token) => (
+        <CollateralTokenRow
+          key={token.address}
+          token={token}
+          onClick={() => {}}
+        />
+      ))}
     </div>
   );
 };
