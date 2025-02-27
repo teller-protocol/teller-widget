9eba68d96c302b95be145bdd8b5631333413e148
andreskebe <andres@teller.org>
2025-02-27 09:09:24 -0400

    Update media query

--- src/hooks/useIsMobile.ts
+++ src/hooks/useIsMobile.ts
@@ -5,7 +5,7 @@ const useIsMobile = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth <= 768);
+      setIsMobile(window.innerWidth <= 640);
     };
 
     window.addEventListener("resize", handleResize);
