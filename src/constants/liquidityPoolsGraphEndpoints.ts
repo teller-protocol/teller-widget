1b57d65f5e36b9f8966821b285e07c6f028fb8aa
andreskebe <andres@teller.org>
2025-06-04 13:55:14 -0400

    Update polygon network

--- src/constants/liquidityPoolsGraphEndpoints.ts
+++ src/constants/liquidityPoolsGraphEndpoints.ts
@@ -5,7 +5,7 @@ export const getLiquidityPoolsGraphEndpoint = (chainId: number) => {
     [chainId: number]: string | undefined;
   } = {
     [mainnet.id]: `https://hasura-mainnet.nfteller.org/v1/graphql`,
-    [polygon.id]: `https://api.studio.thegraph.com/query/36377/tellerv2-lender-groups-polygon/version/latest`,
+    [polygon.id]: `https://hasura-polygon.nfteller.org/v1/graphql`,
     [arbitrum.id]: `https://hasura-arbitrum.nfteller.org/v1/graphql`,
     [base.id]: `https://hasura-base.nfteller.org/v1/graphql`,
   };
