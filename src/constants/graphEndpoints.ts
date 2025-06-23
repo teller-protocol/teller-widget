7cc3083a8ecbe8f23d6782c98a892c649c281d0a
andreskebe <andres@teller.org>
2025-06-23 18:05:41 -0400

    Update arb graph endpoint

--- src/constants/graphEndpoints.ts
+++ src/constants/graphEndpoints.ts
@@ -4,7 +4,7 @@ export const getGraphEndpointWithKey = (apiKey: string, chainId: number) => {
   const GRAPH_ENDPOINTS: { [chainId: number]: string | undefined } = {
     [mainnet.id]: `https://gateway-arbitrum.network.thegraph.com/api/${apiKey}/subgraphs/id/B6FW3z7yLTJKVuqz6kLDJAwJru1T89j4ww5JfY3GYX8F`,
     [polygon.id]: `https://gateway-arbitrum.network.thegraph.com/api/${apiKey}/subgraphs/id/8bjHtQZ9PZUMQAbCGJw5Zx2SbZZY2LQz8WH3rURzN5do`,
-    [arbitrum.id]: `https://api.studio.thegraph.com/query/51204/tellerv2-arbitrum/v0.4.21-16`,
+    [arbitrum.id]: `https://gateway.thegraph.com/api/${apiKey}/subgraphs/id/F2Cgx4q4ATiopuZ13nr1EMKmZXwfAdevF3EujqfayK7a`,
     [base.id]: `https://gateway-arbitrum.network.thegraph.com/api/${apiKey}/subgraphs/id/ArZjkaosc5rmBoYqprFWomvnjKSFMGkPFD7yuaZGuqQo`,
   };
 
